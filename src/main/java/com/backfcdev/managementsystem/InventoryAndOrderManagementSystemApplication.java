de7cfc167c84b40379a403d58e720ec1b72b9a8a
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-06-23 10:15:14 -0500

    added project backend

---
+++ src/main/java/com/backfcdev/managementsystem/InventoryAndOrderManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.backfcdev.managementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryAndOrderManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryAndOrderManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backfcdev/managementsystem/InventoryAndOrderManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backfcdev.managementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryAndOrderManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
