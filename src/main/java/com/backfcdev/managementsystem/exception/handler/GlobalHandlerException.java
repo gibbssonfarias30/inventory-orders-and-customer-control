331611c8fe758e5ae1f1f1366e8cfacf8776753f
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-06-24 10:56:06 -0500

    static import of HttpStatus

--- src/main/java/com/backfcdev/managementsystem/exception/handler/GlobalHandlerException.java
+++ src/main/java/com/backfcdev/managementsystem/exception/handler/GlobalHandlerException.java
@@ -6,7 +6,6 @@ import com.backfcdev.managementsystem.exception.InsufficientStockException;
 import com.backfcdev.managementsystem.exception.MediaFileNotFoundException;
 import com.backfcdev.managementsystem.exception.ModelNotFoundException;
 import com.backfcdev.managementsystem.exception.StorageException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,6 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.net.URI;
 import java.time.Instant;
 
+import static org.springframework.http.HttpStatus.*;
 
 
 @RestControllerAdvice
@@ -21,7 +21,7 @@ public class GlobalHandlerException {
 
     @ExceptionHandler(ModelNotFoundException.class)
     ProblemDetail handleModelNotFoundException(ModelNotFoundException ex){
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(NOT_FOUND, ex.getMessage());
         problemDetail.setTitle("Not Found");
         problemDetail.setType(URI.create("/not-found"));
         problemDetail.setProperty("timestamp", Instant.now());
@@ -30,7 +30,7 @@ public class GlobalHandlerException {
 
     @ExceptionHandler(StorageException.class)
     ProblemDetail handleStorageException(StorageException ex){
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.EXPECTATION_FAILED, ex.getMessage());
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(EXPECTATION_FAILED, ex.getMessage());
         problemDetail.setTitle("Expectation Failed");
         problemDetail.setType(URI.create("/expectation-failed"));
         problemDetail.setProperty("timestamp", Instant.now());
@@ -39,7 +39,7 @@ public class GlobalHandlerException {
 
     @ExceptionHandler(MediaFileNotFoundException.class)
     ProblemDetail handleMediaFileNotFoundException(MediaFileNotFoundException ex){
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(NOT_FOUND, ex.getMessage());
         problemDetail.setTitle("Not Found");
         problemDetail.setType(URI.create("/not-found"));
         problemDetail.setProperty("timestamp", Instant.now());
@@ -48,7 +48,7 @@ public class GlobalHandlerException {
 
     @ExceptionHandler(InsufficientStockException.class)
     ProblemDetail handleInsufficientStockException(InsufficientStockException ex){
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.getMessage());
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(CONFLICT, ex.getMessage());
         problemDetail.setTitle("Conflict");
         problemDetail.setDetail("There is not enough stock for this product");
         problemDetail.setType(URI.create("/conflict"));
