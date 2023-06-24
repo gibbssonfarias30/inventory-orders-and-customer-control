fccfb31475f61cbc118b37a74968602d6c4b9e3d
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-06-23 21:56:23 -0500

    extending the generated ICRUD interface, as well as adding different methods

---
+++ src/main/java/com/backfcdev/managementsystem/service/IProductService.java
@@ -0,0 +1,15 @@
+package com.backfcdev.managementsystem.service;
+
+import com.backfcdev.managementsystem.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IProductService extends ICRUD<Product, Integer> {
+    Page<Product> findByName(String name, Pageable pageable);
+    Page<Product> findByArgs(final Optional<String> name, final Optional<Double> price, Pageable pageable);
+    List<Product> findByStockLessThan(int amount);
+    List<Product> getBestSellingProducts(int amount);
+}
