1af31ce9bd5b7e9a0ce5723cd42b1d5b9cad1aaf
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-06-23 21:51:41 -0500

    added bean modelMapper

---
+++ src/main/java/com/backfcdev/managementsystem/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.backfcdev.managementsystem.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AppConfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
