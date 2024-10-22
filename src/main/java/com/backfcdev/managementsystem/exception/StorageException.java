44189f8953e7807ef36bde1a6923ba76af5a2375
gibbssonfarias30 <gibbfarc17@gmail.com>
2024-10-22 09:42:49 -0500

    feat: new constructor message

--- src/main/java/com/backfcdev/managementsystem/exception/StorageException.java
+++ src/main/java/com/backfcdev/managementsystem/exception/StorageException.java
@@ -1,7 +1,12 @@
 package com.backfcdev.managementsystem.exception;
 
+import java.io.IOException;
+
 public class StorageException extends RuntimeException{
     public StorageException(String message){
+        super(message);
+    }
+    public StorageException(String message, IOException e){
         super(message);
     }
 }
