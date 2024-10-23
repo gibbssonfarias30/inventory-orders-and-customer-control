2ad384fea7783aaadc69cbf9f59df98bcbff918e
gibbssonfarias30 <gibbfarc17@gmail.com>
2024-10-22 23:59:49 -0500

    refactor: Remove unused classes

--- src/main/java/com/backfcdev/managementsystem/repository/IClientRepository.java
+++
@@ -1,6 +0,0 @@
-package com.backfcdev.managementsystem.repository;
-
-import com.backfcdev.managementsystem.model.Client;
-
-public interface IClientRepository extends IGenericRepository<Client, Integer> {
-}
--- src/main/java/com/backfcdev/managementsystem/service/IClientService.java
+++
@@ -1,8 +0,0 @@
-package com.backfcdev.managementsystem.service;
-
-import com.backfcdev.managementsystem.dto.request.ClientRequest;
-import com.backfcdev.managementsystem.dto.response.ClientResponse;
-import com.backfcdev.managementsystem.model.Client;
-
-public interface IClientService extends ICRUD<Client, ClientRequest, ClientResponse, Integer> {
-}
